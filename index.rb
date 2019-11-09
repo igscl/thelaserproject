8dccaa5c9fd2dd6036629c639df01a611e9e6c95
Ignacio Giadach <igiadach@gmail.com>
2019-11-09 15:54:34 +1100

    working loop

--- index.rb
+++ index.rb
@@ -1,11 +1,27 @@
 # the laser project v0.1
 require 'io/console'
 system "clear"
-
+selection = ""
+times = 0
 puts "Welcome, select input -> frequency(F) or a vertebrae(V)?"
-selection = STDIN.noecho(&:getch).chomp.to_s().upcase
-    if selection == "F"
-        puts "hello"
-    elsif selection == "V"
-        puts "goodbye"
+while selection = STDIN.noecho(&:getch).chomp.to_s().upcase
+    begin
+        if selection == "F"
+            puts "hello"
+            break
+        elsif selection == "V"
+            puts "goodbye"
+            break
+        else
+            if times < 5
+                puts "Sorry, I did not understand that, please select again"
+                times +=1
+            else
+                puts "Are you stupid? I already told you"
+                times = 0
+            end
+        end
+    rescue
+        puts "Sorry, I did not understand that."
     end
+end
