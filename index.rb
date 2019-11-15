65dd4af9d7adfd1bbef5b080775e72a406e455bd
Ignacio Giadach <igiadach@gmail.com>
2019-11-15 11:57:31 +1100

    before upgrading

--- index.rb
+++ index.rb
@@ -40,8 +40,8 @@ def find_userv (ha, input)
     end
     unless found_usersv.length < 1
         for i in found_usersv do
-            puts "Frequency #{i["frec"]} and #{i["location"]} in Central Nervous System"
-            puts "#{i["vert"]} relates to #{i["ver_rel"]}"
+            puts "Frequency #{i["frec"]} and #{i["location"]} in Central Nervous System".colorize(:green)
+            puts "#{i["vert"]} relates to #{i["ver_rel"]}".colorize(:green)
         end
     else 
         return "Sorry, could not find #{vsearch}"
@@ -50,9 +50,9 @@ end
 ####
 ### Animation
 def animation1
-    5.times do
+    4.times do
       i = 1
-      while i < 3
+      while i < 7
         print "\033[2J"
         File.foreach("./animation/#{i}.rb") { |f| puts f }
         sleep(0.3)
@@ -136,54 +136,41 @@ elsif vert == true
         puts "Vertebrae #{vsearch} data:"
         puts
         find_userv(ha, vsearch)
-        rescue
-            puts "Sorry, could not find #{vsearch}, please try again"
-    end
-    puts
-    puts "Want to see the map? Y/N"
-    ### MAP SELECTION
-    timesyesno = 0
-    while yesno = STDIN.noecho(&:getch).chomp.to_s().upcase
-        begin
-            if yesno == "Y"
-                animation1
-                # puts
-                # puts
-                # puts "     /~\\"
-                # puts "    (O O)"
-                # puts "    _\\=/_"
-                # puts "   /  _  \\"
-                # puts " ||  \\_/  ||"
-                # puts " || |\\ /| ||"
-                # puts "  # \\_ _/ #"
-                # puts "    | | |"
-                # puts "    | | |"
-                # puts "    []|[]"
-                # puts "    | | |"
-                # puts "   /_] [_\\"
-                # puts
-                # puts
-                frec = true
-                break
-            elsif yesno == "N"
-                puts
-                vert = true
-                break
-            else
-                if timesyesno < 5
-                    puts "Sorry, I did not understand that, please select again"
-                    timesyesno +=1
-                else
-                    puts "It seems to me you're not understanding, goodbye"
+        puts
+        puts "Want to see the map? Y/N"
+        ### MAP SELECTION
+        timesyesno = 0
+        while yesno = STDIN.noecho(&:getch).chomp.to_s().upcase
+            begin
+                if yesno == "Y"
+                    animation1
+                    frec = true
                     break
+                elsif yesno == "N"
+                    puts
+                    vert = true
+                    break
+                else
+                    if timesyesno < 5
+                        puts "Sorry, I did not understand that, please select again"
+                        timesyesno +=1
+                    else
+                        system "clear"
+                        puts "It seems to me you're not understanding, goodbye"
+                        break
+                    end
                 end
+            rescue
+                puts "Sorry, something broke."
+                break
             end
-        rescue
-            puts "Sorry, something broke."
-            break
         end
+        ###
+    
+        rescue
+            puts "Sorry, could not find #{vsearch}"
+            puts
     end
-    ###
 
 else
     puts "Ups. Something went wrong"
